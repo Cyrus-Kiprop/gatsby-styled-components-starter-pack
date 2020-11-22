c9fcf9a4b541f09e2ee24107aafcb279a1723797
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-11-22 11:28:42 +0300

    configure global base styles and resets

---
+++ src/theme/global-style.js
@@ -0,0 +1,9 @@
+// all the base global styles live in this directory
+// start by adding some resets
+
+import { createGlobalStyle } from "styled-components"
+import reset from "styled-reset"
+
+export const globalStyle = createGlobalStyle`
+${reset}
+`
