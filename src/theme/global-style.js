aad44351d72ed21dbfd05ea98515eb15ae02ac7e
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-11-22 11:47:16 +0300

    add smooth scroll, base font, color, height and letter spacing, bgcolor

--- src/theme/global-style.js
+++ src/theme/global-style.js
@@ -10,12 +10,22 @@ ${reset}
 
 html {
     box-sizing: border-box;
---webkit-box-sizing: border-box;
---moz-box-sizing: border-box;
+    --webkit-box-sizing: border-box;
+    --moz-box-sizing: border-box;
+    scroll-behavior: smooth;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 16px;
+    color: '#1a202c';
+
 }
 
 *, *:before, *:after {
     box-sizing: inherit;
 }
 
+body {
+    line-height: 1.5;
+    letter-spacing: 0;
+    background-color: '#f7fafc';
+}
 `
