a7c06f41510d65b3e78d742277dbee560a754753
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-11-22 13:11:41 +0300

    use the shared elements on the index.js file

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,16 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { H1 } from "../components/page-elements/h1"
+import { P } from "../components/page-elements/p"
 
 const IndexPage = () => (
   // <Layout>
   <>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <H1>Hi people</H1>
+    <P>Welcome to your new Gatsby site.</P>
+    <P>Now go build something great.</P>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
